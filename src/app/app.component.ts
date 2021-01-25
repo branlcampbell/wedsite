0f5165e090fba1ee9bb07d98b1bc0ba0dc4850a0
branlcampbell <branlcampbell@gmail.com>
2021-01-25 17:14:20 -0500

    Change elements and styles for better responsive design.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  animations: [
+    trigger('contentState', [
+      state('normal', style({
+        'opacity': 1
+      })),
+      state('faded', style({
+        'opacity': 0
+      })),
+      transition('normal <=> faded', animate(500))
+    ])
+  ]
 })
 export class AppComponent {
+  state = 'normal';
   title = 'frontend';
 }
